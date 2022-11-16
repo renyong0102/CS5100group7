f92cd5e2cd2dba05bc4c967d4d70bfb4ba682690
YUCEN CAO <caoyucen@gmail.com>
2022-11-16 15:24:08 -0800

    change the home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import Banner from "./banner";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
 import Market from "./market";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import HomeComponent from "./home";
 import MarketComponent from "./market";
-import Nav from "./nav";
+import NavbarContent from "./nav";
 
 function App() {
   return (
       <BrowserRouter>
           <div className="container">
-              <Nav/>
+              <NavbarContent/>
               <Routes>
                   <Route index path="/" element={<HomeComponent/>}/>
                   <Route index path="/home/*" element={<HomeComponent/>}/>
