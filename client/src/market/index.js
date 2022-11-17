119556b2adc561f5cba3a48ccf1c6ea2b0c008d8
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-11-17 01:15:21 -0800

    market page

--- client/src/market/index.js
+++ client/src/market/index.js
@@ -1,11 +1,17 @@
+import Search from "./search";
+import DatasetComponent from "./dataset";
+
 function MarketComponent() {
     return(
         <div>
             <div className="position-relative mb-2">
                 <img src="/images/banner.jpg" className="w-100"/>
-                <h1 className="position-absolute wd-nudge-up top">
-                    Market</h1>
+
             </div>
+            <h1 className="text-white">
+                Market</h1>
+            <Search/>
+            <DatasetComponent/>
         </div>
     );
 }
