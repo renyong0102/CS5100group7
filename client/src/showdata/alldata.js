8b8a3d19fe01a91a0f3b2883029ccf6529ffe9ea
YUCEN CAO <caoyucen@gmail.com>
2022-12-09 16:21:05 -0800

    add all data table

---
+++ client/src/showdata/alldata.js
@@ -0,0 +1,27 @@
+function AllDataComponent() {
+    return(
+        <div>
+            <table className="table table-dark">
+                <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Open</th>
+                    <th scope="col">High</th>
+                    <th scope="col">Low</th>
+                    <th scope="col">Close</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="table-dark">2017-11-09</th>
+                    <td>308.644989</td>
+                    <td>329.451996</td>
+                    <td>307.056000</td>
+                    <td>320.884003</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+export default AllDataComponent;
