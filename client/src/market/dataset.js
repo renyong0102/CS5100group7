d8e6e53c30c4141e18aba3c7696674c2da0fa8f8
YUCEN CAO <caoyucen@gmail.com>
2022-12-11 19:41:04 -0800

    add stock infos to the table

--- client/src/market/dataset.js
+++ client/src/market/dataset.js
@@ -1,57 +1,61 @@
-function DatasetComponent() {
-    return(
-        <div>
+import {useEffect, useState} from "react";
 
+function DatasetComponent() {
+    const [allData, setAllData] = useState('');
+    const [stockInfos, setStockInfos] = useState([]);
+    var txtFile = new XMLHttpRequest();
+    txtFile.open("GET", "https://raw.githubusercontent.com/Dabaiee/CS5100group7/main/client/src/market/dataset.json", true);
+    txtFile.send()
+    txtFile.onreadystatechange = function () {
+        // console.log("txtFile.readyState = " + txtFile.readyState)
+        // console.log("txtFile.status = " + txtFile.status)
+        if (txtFile.readyState === 4 && txtFile.status === 200) {
+            setAllData(txtFile.responseText);
+        }
+    };
+
+    useEffect(() => {
+        if (allData) {
+            //console.log("get all the Data");
+            //console.log(allData);
+            //const myArr = JSON.parse(allData);
+            setStockInfos(JSON.parse(allData))
+        }
+    }, [allData]);
+
+    useEffect(() =>{
+        if(stockInfos){
+            console.log(stockInfos)
+            console.log("stockInfos !!!!!!!")
+        }
+
+    }, [stockInfos])
+
+    return (
+        <div>
             <table className="table table-dark">
                 <thead>
                 <tr>
                     <th scope="col">Asset</th>
-                    <th scope="col">Market cap</th>
-                    <th scope="col">Last price</th>
-                    <th scope="col">24h change</th>
+                    <th scope="col">MARKET CAP</th>
+                    <th scope="col">AVG VOLUME</th>
+                    <th scope="col">PRIMARY EXCHANGE</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr className="table-active">
-                    <th scope="row">AAPL</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
+                {stockInfos.map((stockInfo) => (
+                    <tr className="table-active" key = {stockInfo.asset}>
 
-                </tr>
-                <tr>
-                    <th scope="table-dark">AMZN</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                </tr>
-                <tr className="table-dark">
-                    <th scope="row">ETH-USD</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                </tr>
-                <tr className="table-dark">
-                    <th scope="row">META</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                </tr>
-                <tr className="table-dark">
-                    <th scope="row">NFLX</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                </tr>
-                <tr className="table-dark">
-                    <th scope="row">TSLA</th>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                    <td>Column content</td>
-                </tr>
+                        <td>{stockInfo.asset}</td>
+                        <td>{stockInfo.marketCap}</td>
+                        <td>{stockInfo.avgVOLUME}</td>
+                        <td>{stockInfo.primaryEXCHANGE}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
     );
 }
+
 export default DatasetComponent;
